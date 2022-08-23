3da1f5a3920b9ad2d804c8199b3059437069f01a
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-08-23 22:09:31 +0400

    Initial commit

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,20 @@
+package data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DataGenerator {
+//    private DataGenerator() {
+    }
+
+//    public String generateDate(int days) {
+//        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
+//        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+//        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//    }
+//}
+//    public String generateDate(int days) {
+//
+//        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//
+//    }
---
+++ src/test/java/test/CardFormTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import data.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.nio.channels.FileChannel.open;
+
+public class CardFormTest {
+
+    @Test
+    void shouldFillInAllTheFieldsCorrectly() {
+//        open("http://localhost:9999/");
+//        $("[data-test-id='city'] input").setValue("Самара");
+//        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+//        $("[data-test-id='name'] input").setValue("Петров Виталий");
+//        $("[data-test-id='phone'] input").setValue("+79179778855");
+//        $("[data-test-id='agreement']").click();
+//        $(".button").click();
+//        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
+//        $("[data-test-id='notification'] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + formatter.format(nextDate)));
+//    }
+}
